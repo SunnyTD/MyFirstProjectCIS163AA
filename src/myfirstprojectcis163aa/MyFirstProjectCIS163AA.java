44709a93714642eb8c96b790431d13ff1b5256c0
Daniel <djplotzke@gmail.com>
2017-10-12 02:34:29 -0700

    Initial commit

---
+++ src/myfirstprojectcis163aa/MyFirstProjectCIS163AA.java
@@ -0,0 +1,20 @@
+/*
+ Daniel Plotzke
+9/14/17
+CIS163AA #26008
+Week 2
+Description here....!!!!!
+ */
+package myfirstprojectcis163aa;
+
+
+public class MyFirstProjectCIS163AA 
+{
+
+    
+    public static void main(String[] args)
+    {
+      System.out.println("Hello, World!");
+    }
+    
+}
